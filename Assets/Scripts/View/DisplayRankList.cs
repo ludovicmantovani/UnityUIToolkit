f55326b56881b3a1967be9e7076a13ffa11bd9b6
ludovicmantovani <ludovic.mantov@gmail.com>
2023-03-27 22:05:10 +0200

    [ADD] : Basic list view to show ranking

---
+++ Assets/Scripts/View/DisplayRankList.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using UnityEngine;
+using System;
+
+namespace View
+{
+    public class DisplayRankList : MonoBehaviour
+    {
+        [Header("Visual Element")]
+        [SerializeField] private string listViewName = "List";
+        
+        private UIDocument uiDocument = null;
+        private ListView listView = null;
+        
+        private void OnEnable()
+        {
+            if (TryGetComponent<UIDocument>(out uiDocument))
+            {
+                listView = uiDocument.rootVisualElement.Q<ListView>(listViewName);
+            }
+            Controller.RankManager.onUpdateRanking += RefreshListView;
+        }
+        private void OnDisable()
+        {
+            Controller.RankManager.onUpdateRanking -= RefreshListView;
+        }
+
+        /// <summary>
+        /// Called after ranklist update.
+        /// Display this new rank list.
+        /// </summary>
+        /// <param name="ranklist"></param>
+        private void RefreshListView(List<string> ranklist)
+        {
+            if (listView == null) return;
+            listView.itemsSource = ranklist;
+            listView.makeItem = () => new Label();
+            listView.bindItem = (elem, index) => (elem as Label).text = (index + 1).ToString() + " " + ranklist[index];
+        }
+    }
+}
